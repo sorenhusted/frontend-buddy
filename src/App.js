28cde24c5c84af02539b065bb3a916d86208f3c5
Søren Beier Husted <soren.husted@akqa.com>
2019-09-25 21:48:14 +0200

    Round Robin algorithm appled

--- src/App.js
+++ src/App.js
@@ -3,36 +3,49 @@ import './App.css';
 
 import frontenders from './data'; 
 
-const halfSize = Math.floor(frontenders.length/2);
+const maxIndex = frontenders.length - 1;
 
 function App() {
   
-  const [offset, setOffset] = useState(0);
+  const [round, setRound] = useState(1);
   const [buddies, setBuddies] = useState([]);
 
 
   const createList = () => {
+    // The new set of buddy pairs
     let newBuddies = [];
-    const list = [...frontenders];
 
-    const getNextPair = iteration => {
-      const nextPair = [
-        list[0],
-        list[1],
-      ];
-    }
+    // Skip the first index, as it is always used for the first pair
+    const findIndex = index => index % maxIndex || maxIndex;
     
-    for (let i = 0; i < halfSize; i++) {
+    // Round Robin tournament: https://en.wikipedia.org/wiki/Round-robin_tournament
+    for (let i = 0; i < Math.floor(frontenders.length/2); i++) {
+      let pair = [];
+      
+      // The first pair in each round follow a different pattern
+      if (i === 0) {
+        pair = [
+          // The first person is fixed
+          frontenders[0],
+          frontenders[round],
+        ];
+      }
+      else {
+        pair = [
+          frontenders[findIndex(i + round)],
+          frontenders[findIndex(maxIndex - i + round)],
+        ];
+      }
+
       newBuddies = [
         ...newBuddies,
-        [
-          frontenders[i],
-          frontenders[((i + offset + halfSize) % frontenders.length)],
-        ],
+        pair,
       ];
     }
 
-    setOffset((offset + 1) % halfSize);
+    // It takes (n-1) rounds to get back to the starting configuration
+    setRound((round % (frontenders.length - 1)) + 1);
+
     setBuddies(newBuddies);
   }
 
@@ -43,7 +56,7 @@ function App() {
         { buddies.map(pair => (
           <div className="list__pair" key={pair[0]}>
             { pair.map(member => (
-              <div className="list__member" key={member}>{ member }</div>
+              <div className="list__member" key={member}>{ member } ({ frontenders.indexOf(member) })</div>
             )) }
           </div>
         ))}
