bfe79bbd196d218b022715d216d8efad03888829
Søren Beier Husted <soren.husted@akqa.com>
2019-09-25 08:55:44 +0200

    Work in progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState }  from 'react';
 import './App.css';
 
+import frontenders from './data'; 
+
+const halfSize = Math.floor(frontenders.length/2);
+
 function App() {
+  
+  const [offset, setOffset] = useState(0);
+  const [buddies, setBuddies] = useState([]);
+
+
+  const createList = () => {
+    let newBuddies = [];
+    const list = [...frontenders];
+
+    const getNextPair = iteration => {
+      const nextPair = [
+        list[0],
+        list[1],
+      ];
+    }
+    
+    for (let i = 0; i < halfSize; i++) {
+      newBuddies = [
+        ...newBuddies,
+        [
+          frontenders[i],
+          frontenders[((i + offset + halfSize) % frontenders.length)],
+        ],
+      ];
+    }
+
+    setOffset((offset + 1) % halfSize);
+    setBuddies(newBuddies);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={createList}>Get new teams</button>
+      <div className="list">
+        { buddies.map(pair => (
+          <div className="list__pair" key={pair[0]}>
+            { pair.map(member => (
+              <div className="list__member" key={member}>{ member }</div>
+            )) }
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
